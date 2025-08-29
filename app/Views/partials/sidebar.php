2995efdaba764d4afed95f571853d1fb1fe45428
agustinitn <agustinsetya38@gmail.com>
2025-08-29 14:04:08 +0700

    Remove unused code

--- app/Views/partials/sidebar.php
+++ app/Views/partials/sidebar.php
@@ -1,11 +1,6 @@
-<!-- ========== Left Sidebar Start ========== -->
 <div class="vertical-menu">
-
     <div data-simplebar class="h-100">
-
-        <!--- Sidemenu -->
         <div id="sidebar-menu">
-            <!-- Left Menu Start -->
             <ul class="metismenu list-unstyled" id="side-menu">
                 <li class="menu-title" data-key="t-menu"><?= lang('Files.Menu') ?></li>
 
@@ -103,7 +98,5 @@
                 </li>
             </ul>
         </div>
-        <!-- Sidebar -->
     </div>
 </div>
-<!-- Left Sidebar End -->
