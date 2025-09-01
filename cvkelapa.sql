2c0de9add626a43ca3a11849bbaca2abe23c6442
Agustin Setyo Rini <agustinsetya38@gmail.com>
2025-09-01 07:57:33 +0700

    Update database

--- cvkelapa.sql
+++ cvkelapa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 29, 2025 at 02:53 AM
+-- Generation Time: Sep 01, 2025 at 12:56 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.3.3
 
@@ -20,6 +20,49 @@ SET time_zone = "+00:00";
 --
 -- Database: `cvkelapa`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mt_bonus`
+--
+
+CREATE TABLE `mt_bonus` (
+  `mt_bonus_id` int(10) NOT NULL,
+  `kd_pegawai` char(10) NOT NULL,
+  `biaya` float NOT NULL,
+  `is_stat_gaji` tinyint(1) NOT NULL DEFAULT 0,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `created_by` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mt_gaji`
+--
+
+CREATE TABLE `mt_gaji` (
+  `mt_gaji_id` int(10) NOT NULL,
+  `periode` date NOT NULL,
+  `kd_pegawai` char(10) NOT NULL,
+  `upah_produksi` float NOT NULL,
+  `bonus` float NOT NULL,
+  `total_gaji_bersih` float NOT NULL,
+  `status_pembayaran` enum('BELUM_BAYAR','PENDING','DIBAYAR','GAGAL') NOT NULL DEFAULT 'BELUM_BAYAR',
+  `tg_pembayaran` date DEFAULT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `created_by` varchar(100) NOT NULL,
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `updated_by` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `mt_gaji`
+--
+
+INSERT INTO `mt_gaji` (`mt_gaji_id`, `periode`, `kd_pegawai`, `upah_produksi`, `bonus`, `total_gaji_bersih`, `status_pembayaran`, `tg_pembayaran`, `created_at`, `created_by`, `updated_at`, `updated_by`) VALUES
+(1, '2025-08-01', '9000000586', 1500000, 100000, 2100000, 'BELUM_BAYAR', '0000-00-00', '2025-08-31 22:01:01', 'agustin@gmail.com', '2025-08-31 22:01:01', '');
 
 -- --------------------------------------------------------
 
@@ -69,10 +112,38 @@ CREATE TABLE `mt_pembelian` (
 
 INSERT INTO `mt_pembelian` (`mt_pembelian_id`, `tg_pembelian`, `gudang_id`, `berat_kelapa`, `created_at`, `created_by`, `updated_at`, `updated_by`) VALUES
 (1, '2025-08-21', 1, 57, '2025-08-21 09:35:47', 'agustin@gmail.com', '2025-08-28 10:44:13', 'agustin@gmail.com'),
-(2, '2025-08-28', 1, 20, '2025-08-28 10:47:47', '', '2025-08-28 10:47:47', ''),
-(3, '2025-08-29', 3, 520, '2025-08-29 02:11:48', '', '2025-08-29 02:11:48', ''),
-(4, '2025-08-29', 2, 420, '2025-08-29 02:12:03', '', '2025-08-29 02:12:03', ''),
-(5, '2025-08-26', 3, 145, '2025-08-29 02:12:15', '', '2025-08-29 02:12:28', 'agustin@gmail.com');
+(2, '2025-08-28', 3, 275, '2025-08-28 10:47:47', '', '2025-08-29 01:11:57', 'agustin@gmail.com'),
+(3, '2025-08-28', 2, 28, '2025-08-28 11:31:52', '', '2025-08-29 00:12:30', 'agustin@gmail.com'),
+(4, '2025-08-29', 4, 520, '2025-08-29 01:11:44', '', '2025-08-29 01:11:44', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mt_pengeluaran`
+--
+
+CREATE TABLE `mt_pengeluaran` (
+  `mt_pengeluaran_id` int(11) NOT NULL,
+  `tg_pengeluaran` date NOT NULL,
+  `ktg_pengeluaran_id` int(10) NOT NULL,
+  `gudang_id` int(10) NOT NULL,
+  `jumlah` int(10) NOT NULL,
+  `biaya` float NOT NULL,
+  `kd_pegawai` char(10) NOT NULL,
+  `status` enum('BELUM_BAYAR','SUDAH_BAYAR') NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `created_by` varchar(100) NOT NULL,
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `updated_by` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `mt_pengeluaran`
+--
+
+INSERT INTO `mt_pengeluaran` (`mt_pengeluaran_id`, `tg_pengeluaran`, `ktg_pengeluaran_id`, `gudang_id`, `jumlah`, `biaya`, `kd_pegawai`, `status`, `created_at`, `created_by`, `updated_at`, `updated_by`) VALUES
+(2, '2025-08-31', 1, 3, 11, 1100000, '9000000123', 'SUDAH_BAYAR', '2025-08-31 11:03:08', 'agustin@gmail.com', '2025-08-31 12:29:27', 'agustin@gmail.com'),
+(3, '2025-08-31', 2, 2, 5, 400000, '9000000123', 'BELUM_BAYAR', '2025-08-31 12:29:47', 'agustin@gmail.com', '2025-08-31 12:33:44', 'agustin@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -82,11 +153,13 @@ INSERT INTO `mt_pembelian` (`mt_pembelian_id`, `tg_pembelian`, `gudang_id`, `ber
 
 CREATE TABLE `mt_pengolahan` (
   `mt_pengolahan_id` int(10) NOT NULL,
-  `tg_pengolahan` date NOT NULL,
+  `tg_pengolahan` date NOT NULL DEFAULT current_timestamp(),
   `gudang_id` int(10) NOT NULL,
   `kd_pegawai` char(10) NOT NULL,
   `berat_daging` int(10) NOT NULL,
   `berat_kopra` int(10) NOT NULL,
+  `bonus` float NOT NULL,
+  `is_stat_gaji` tinyint(1) NOT NULL DEFAULT 0,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `created_by` varchar(100) NOT NULL,
   `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
@@ -97,21 +170,21 @@ CREATE TABLE `mt_pengolahan` (
 -- Dumping data for table `mt_pengolahan`
 --
 
-INSERT INTO `mt_pengolahan` (`mt_pengolahan_id`, `tg_pengolahan`, `gudang_id`, `kd_pegawai`, `berat_daging`, `berat_kopra`, `created_at`, `created_by`, `updated_at`, `updated_by`) VALUES
-(3, '2025-08-26', 1, '9000000123', 250, 500, '2025-08-28 18:12:38', 'agustin@gmail.com', '2025-08-28 18:16:09', ''),
-(4, '2025-08-19', 4, '9000000586', 200, 482, '2025-08-28 18:12:38', 'agustin@gmail.com', '2025-08-28 18:16:09', ''),
-(5, '2025-08-20', 1, '9000000123', 250, 500, '2025-08-28 18:14:06', 'agustin@gmail.com', '2025-08-28 18:16:09', ''),
-(6, '2025-08-20', 4, '9000000586', 200, 482, '2025-08-28 18:14:06', 'agustin@gmail.com', '2025-08-28 18:16:09', ''),
-(7, '2025-08-12', 1, '9000000123', 250, 500, '2025-08-28 18:14:06', 'agustin@gmail.com', '2025-08-28 18:16:09', ''),
-(8, '2025-08-14', 4, '9000000586', 200, 482, '2025-08-28 18:14:06', 'agustin@gmail.com', '2025-08-28 18:16:09', ''),
-(9, '2025-08-14', 1, '9000000123', 250, 500, '2025-08-28 18:14:06', 'agustin@gmail.com', '2025-08-28 18:16:09', ''),
-(10, '2025-08-06', 4, '9000000586', 200, 482, '2025-08-28 18:14:06', 'agustin@gmail.com', '2025-08-28 18:16:09', ''),
-(11, '2025-08-07', 2, '9000000123', 250, 500, '2025-08-28 18:14:06', 'agustin@gmail.com', '2025-08-28 18:16:09', ''),
-(12, '2025-08-12', 3, '9000000586', 200, 482, '2025-08-28 18:14:06', 'agustin@gmail.com', '2025-08-28 18:16:09', ''),
-(13, '2025-08-28', 4, '9000000123', 250, 500, '2025-08-28 18:14:06', 'agustin@gmail.com', '2025-08-28 18:14:06', ''),
-(14, '2025-08-28', 3, '9000000586', 200, 482, '2025-08-28 18:14:06', 'agustin@gmail.com', '2025-08-28 18:14:06', ''),
-(15, '2025-08-28', 6, '9000000123', 250, 500, '2025-08-28 18:14:55', 'agustin@gmail.com', '2025-08-28 18:14:55', ''),
-(16, '2025-08-28', 4, '9000000586', 200, 482, '2025-08-28 18:14:55', 'agustin@gmail.com', '2025-08-28 18:14:55', '');
+INSERT INTO `mt_pengolahan` (`mt_pengolahan_id`, `tg_pengolahan`, `gudang_id`, `kd_pegawai`, `berat_daging`, `berat_kopra`, `bonus`, `is_stat_gaji`, `created_at`, `created_by`, `updated_at`, `updated_by`) VALUES
+(3, '2025-08-26', 1, '9000000123', 250, 500, 0, 0, '2025-08-28 18:12:38', 'agustin@gmail.com', '2025-08-28 18:16:09', ''),
+(4, '2025-08-19', 4, '9000000586', 200, 482, 0, 0, '2025-08-28 18:12:38', 'agustin@gmail.com', '2025-08-28 18:16:09', ''),
+(5, '2025-08-20', 1, '9000000123', 250, 500, 0, 0, '2025-08-28 18:14:06', 'agustin@gmail.com', '2025-08-28 18:16:09', ''),
+(6, '2025-08-20', 4, '9000000586', 200, 482, 0, 0, '2025-08-28 18:14:06', 'agustin@gmail.com', '2025-08-28 18:16:09', ''),
+(7, '2025-08-12', 1, '9000000123', 250, 500, 0, 0, '2025-08-28 18:14:06', 'agustin@gmail.com', '2025-08-28 18:16:09', ''),
+(8, '2025-08-14', 4, '9000000586', 200, 482, 0, 0, '2025-08-28 18:14:06', 'agustin@gmail.com', '2025-08-28 18:16:09', ''),
+(9, '2025-08-14', 1, '9000000123', 250, 500, 0, 0, '2025-08-28 18:14:06', 'agustin@gmail.com', '2025-08-28 18:16:09', ''),
+(10, '2025-08-06', 4, '9000000586', 200, 482, 0, 0, '2025-08-28 18:14:06', 'agustin@gmail.com', '2025-08-28 18:16:09', ''),
+(11, '2025-08-07', 2, '9000000123', 250, 500, 0, 0, '2025-08-28 18:14:06', 'agustin@gmail.com', '2025-08-28 18:16:09', ''),
+(12, '2025-08-12', 3, '9000000586', 200, 482, 0, 0, '2025-08-28 18:14:06', 'agustin@gmail.com', '2025-08-28 18:16:09', ''),
+(13, '2025-08-28', 4, '9000000123', 250, 500, 0, 0, '2025-08-28 18:14:06', 'agustin@gmail.com', '2025-08-28 18:14:06', ''),
+(14, '2025-08-28', 3, '9000000586', 200, 482, 0, 0, '2025-08-28 18:14:06', 'agustin@gmail.com', '2025-08-28 18:14:06', ''),
+(15, '2025-08-28', 6, '9000000123', 250, 500, 0, 0, '2025-08-28 18:14:55', 'agustin@gmail.com', '2025-08-28 18:14:55', ''),
+(16, '2025-08-28', 4, '9000000586', 200, 482, 0, 0, '2025-08-28 18:14:55', 'agustin@gmail.com', '2025-08-28 18:14:55', '');
 
 -- --------------------------------------------------------
 
@@ -124,7 +197,6 @@ CREATE TABLE `mt_user` (
   `kd_pegawai` char(10) NOT NULL,
   `email` varchar(25) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `status` tinyint(1) NOT NULL DEFAULT 1,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -133,9 +205,9 @@ CREATE TABLE `mt_user` (
 -- Dumping data for table `mt_user`
 --
 
-INSERT INTO `mt_user` (`mt_user_id`, `kd_pegawai`, `email`, `password`, `status`, `created_at`, `updated_at`) VALUES
-(1, '9000000586', 'agustin@gmail.com', '$2y$10$P6bTD/iV6ghdA86GAUEAGe39izhoNIMdQOcQpBGMBoJ/MkRcBsl3e', 1, '2025-08-18 19:55:58', '2025-08-19 11:00:31'),
-(2, '9000000123', 'rian@gmail.com', '$2y$10$vzD.MZ6TWnxSA6/ZuubkxOPbm92dCmDWF80G4Jnt7MTCCIhz6xUlO', 1, '2025-08-18 19:55:58', '2025-08-21 09:32:45');
+INSERT INTO `mt_user` (`mt_user_id`, `kd_pegawai`, `email`, `password`, `created_at`, `updated_at`) VALUES
+(1, '9000000586', 'agustin@gmail.com', '$2y$10$P6bTD/iV6ghdA86GAUEAGe39izhoNIMdQOcQpBGMBoJ/MkRcBsl3e', '2025-08-18 19:55:58', '2025-08-31 02:24:18'),
+(3, '9000000123', 'riansss@gmail.com', '$2y$10$B./3eC7OvNIFPjuwo9e4vOghOECN336caeXDX2PcaxBuUgHBSzWzm', '2025-08-31 02:24:08', '2025-08-31 02:24:08');
 
 -- --------------------------------------------------------
 
@@ -192,6 +264,27 @@ INSERT INTO `m_komponen_gaji` (`m_komponen_gaji_id`, `gudang_id`, `takaran_dagin
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `m_ktg_pengeluaran`
+--
+
+CREATE TABLE `m_ktg_pengeluaran` (
+  `m_ktg_pengeluaran_id` int(10) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `keterangan` text NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `m_ktg_pengeluaran`
+--
+
+INSERT INTO `m_ktg_pengeluaran` (`m_ktg_pengeluaran_id`, `nama`, `keterangan`, `created_at`) VALUES
+(1, 'BBM', 'Solar/BBM untuk kendaraan, genset, forklift, dan lain-lain.', '0000-00-00 00:00:00'),
+(2, 'Perawatan & Perbaikan', 'Servis mesin press, penggantian spare part conveyor, pelumasan mesin, jasa teknisi.', '0000-00-00 00:00:00');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `m_role`
 --
 
@@ -216,6 +309,20 @@ INSERT INTO `m_role` (`m_role_id`, `nama`, `role_scope`) VALUES
 --
 
 --
+-- Indexes for table `mt_bonus`
+--
+ALTER TABLE `mt_bonus`
+  ADD PRIMARY KEY (`mt_bonus_id`),
+  ADD KEY `fk_bonus_pegawai` (`kd_pegawai`);
+
+--
+-- Indexes for table `mt_gaji`
+--
+ALTER TABLE `mt_gaji`
+  ADD PRIMARY KEY (`mt_gaji_id`),
+  ADD KEY `fk_gaji_pegawai` (`kd_pegawai`);
+
+--
 -- Indexes for table `mt_pegawai`
 --
 ALTER TABLE `mt_pegawai`
@@ -229,6 +336,15 @@ ALTER TABLE `mt_pegawai`
 --
 ALTER TABLE `mt_pembelian`
   ADD PRIMARY KEY (`mt_pembelian_id`),
+  ADD KEY `gudang_id` (`gudang_id`);
+
+--
+-- Indexes for table `mt_pengeluaran`
+--
+ALTER TABLE `mt_pengeluaran`
+  ADD PRIMARY KEY (`mt_pengeluaran_id`),
+  ADD KEY `fk_pengeluaran_pegawai` (`kd_pegawai`),
+  ADD KEY `fk_pengeluaran_ktg` (`ktg_pengeluaran_id`),
   ADD KEY `gudang_id` (`gudang_id`);
 
 --
@@ -261,6 +377,12 @@ ALTER TABLE `m_komponen_gaji`
   ADD KEY `gudang_id` (`gudang_id`);
 
 --
+-- Indexes for table `m_ktg_pengeluaran`
+--
+ALTER TABLE `m_ktg_pengeluaran`
+  ADD PRIMARY KEY (`m_ktg_pengeluaran_id`);
+
+--
 -- Indexes for table `m_role`
 --
 ALTER TABLE `m_role`
@@ -269,6 +391,18 @@ ALTER TABLE `m_role`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `mt_bonus`
+--
+ALTER TABLE `mt_bonus`
+  MODIFY `mt_bonus_id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `mt_gaji`
+--
+ALTER TABLE `mt_gaji`
+  MODIFY `mt_gaji_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `mt_pegawai`
@@ -280,7 +414,13 @@ ALTER TABLE `mt_pegawai`
 -- AUTO_INCREMENT for table `mt_pembelian`
 --
 ALTER TABLE `mt_pembelian`
-  MODIFY `mt_pembelian_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `mt_pembelian_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `mt_pengeluaran`
+--
+ALTER TABLE `mt_pengeluaran`
+  MODIFY `mt_pengeluaran_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `mt_pengolahan`
@@ -292,7 +432,7 @@ ALTER TABLE `mt_pengolahan`
 -- AUTO_INCREMENT for table `mt_user`
 --
 ALTER TABLE `mt_user`
-  MODIFY `mt_user_id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `mt_user_id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `m_gudang`
@@ -307,6 +447,12 @@ ALTER TABLE `m_komponen_gaji`
   MODIFY `m_komponen_gaji_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT for table `m_ktg_pengeluaran`
+--
+ALTER TABLE `m_ktg_pengeluaran`
+  MODIFY `m_ktg_pengeluaran_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `m_role`
 --
 ALTER TABLE `m_role`
@@ -315,6 +461,18 @@ ALTER TABLE `m_role`
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `mt_bonus`
+--
+ALTER TABLE `mt_bonus`
+  ADD CONSTRAINT `fk_bonus_pegawai` FOREIGN KEY (`kd_pegawai`) REFERENCES `mt_pegawai` (`kd_pegawai`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `mt_gaji`
+--
+ALTER TABLE `mt_gaji`
+  ADD CONSTRAINT `fk_gaji_pegawai` FOREIGN KEY (`kd_pegawai`) REFERENCES `mt_pegawai` (`kd_pegawai`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `mt_pegawai`
@@ -328,6 +486,14 @@ ALTER TABLE `mt_pegawai`
 --
 ALTER TABLE `mt_pembelian`
   ADD CONSTRAINT `fk_pembelian_gudang` FOREIGN KEY (`gudang_id`) REFERENCES `m_gudang` (`m_gudang_id`);
+
+--
+-- Constraints for table `mt_pengeluaran`
+--
+ALTER TABLE `mt_pengeluaran`
+  ADD CONSTRAINT `fk_pengeluaran_gudang` FOREIGN KEY (`gudang_id`) REFERENCES `m_gudang` (`m_gudang_id`),
+  ADD CONSTRAINT `fk_pengeluaran_ktg` FOREIGN KEY (`ktg_pengeluaran_id`) REFERENCES `m_ktg_pengeluaran` (`m_ktg_pengeluaran_id`),
+  ADD CONSTRAINT `fk_pengeluaran_pegawai` FOREIGN KEY (`kd_pegawai`) REFERENCES `mt_pegawai` (`kd_pegawai`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `mt_pengolahan`
